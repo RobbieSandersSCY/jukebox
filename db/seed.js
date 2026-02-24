cf97d2be558b7e399aaf61da7a25ff538fb6827b
Robbie Sanders <sande290@gmail.com>
2026-02-24 16:33:42 -0500

    add seed functions

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,7 @@
 import db from "#db/client";
+import { createTrack } from "#db/queries/tracks";
+import { createPlaylist } from "#db/queries/playlists";
+import { createPlaylist_tracks } from "#db/queries/playlists_tracks";
 
 await db.connect();
 await seed();
@@ -6,5 +9,19 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
+  for (let i = 1; i <= 30; i++) {
+    const track = await createTrack(
+      "Track " + i,
+      Math.floor(Math.random() * 200 + 90),
+    );
+  }
+  for (let i = 1; i <= 20; i++) {
+    const playlist = await createPlaylist("Playlist " + i, "BLAH BLAH BLAH");
+  }
+  for (let i = 1; i <= 25; i++) {
+    const playlist_track = await createPlaylist_tracks(
+      Math.floor(Math.random() * 20 + 1),
+      Math.floor(Math.random() * 30 + 1),
+    );
+  }
 }
